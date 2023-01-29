a8ca2a0deb4da64aa2289521d9cfdf1f3f05ffe8
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-01-28 17:43:48 -0700

    Set up fields and rules to define Tag and Product Tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,21 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  product_id: {
+    type: DataTypes.INTERGER,
+    foreignKey: product_id,
+  },
+  tag_id: {
+    type: DataTypes.INTERGER,
+    foreignKey: tag_id,
+  },
+
   },
   {
     sequelize,
