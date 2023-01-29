a59ffd95dd2ec15a31c5d44f372e68d3364b45d4
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-01-28 17:09:32 -0700

    deleted 1 env.Ex, updated models Category and index, server.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne({
+  foreignKey: 'Category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
-
+Category.hasMany({
+  foreignKey: 'Product_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany({
+  foreignKey: 'Product_id',
+  foreignKey: 'Tag_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany({
+  foreignKey: 'ProductTag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
